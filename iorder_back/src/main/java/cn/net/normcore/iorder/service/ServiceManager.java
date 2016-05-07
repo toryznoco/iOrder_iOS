d178e71229e1ea4c3926ce558fc91df129588bd3
RenQiang <810625938@qq.com>
2016-05-08 07:45:49 +0800

    2016-05-08 获取附近餐厅列表

--- iorder_back/src/main/java/cn/net/normcore/iorder/service/ServiceManager.java
+++ iorder_back/src/main/java/cn/net/normcore/iorder/service/ServiceManager.java
@@ -1,5 +1,13 @@
 package cn.net.normcore.iorder.service;
 
 public class ServiceManager {
-	
+	private ShopService shopService;
+
+	public ShopService getShopService() {
+		return shopService;
+	}
+
+	public void setShopService(ShopService shopService) {
+		this.shopService = shopService;
+	}
 }
