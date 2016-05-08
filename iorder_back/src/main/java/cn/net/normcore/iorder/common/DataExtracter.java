00e2c104c6a58c636159f4fdec901b973737800a
RenQiang <810625938@qq.com>
2016-05-08 08:17:30 +0800

    delete

--- iorder_back/src/main/java/cn/net/normcore/iorder/common/DataExtracter.java
+++
@@ -1,9 +0,0 @@
-package cn.net.normcore.iorder.common;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Map;
-
-public interface DataExtracter {
-	Map<String, Object> extractData(ResultSet rst) throws SQLException;
-}
