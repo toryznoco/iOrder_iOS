8a4c7a8e92addf512cb14541dc830917a271aa94
ÊòìÊó†Ëß£ <2198105373@qq.com>
2016-05-23 21:34:14 +0800

    退出键盘

--- iOrder_iOS/iOrder/Classes/Home/Controller/IOHomeViewController.m
+++ iOrder_iOS/iOrder/Classes/Home/Controller/IOHomeViewController.m
@@ -151,6 +151,11 @@
     }];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+
 - (void)locatingBtnClick:(UIButton *)btn {
     
 }
