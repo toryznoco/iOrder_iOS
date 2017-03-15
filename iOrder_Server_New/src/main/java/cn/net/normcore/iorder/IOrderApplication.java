a5275456b56a033a147bb9e940fa26035243e30f
Ryanznoco <810625938@qq.com>
2017-03-15 19:58:18 +0800

    初始导入

---
+++ iOrder_Server_New/src/main/java/cn/net/normcore/iorder/IOrderApplication.java
@@ -0,0 +1,18 @@
+package cn.net.normcore.iorder;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.TracingConfig;
+
+/**
+ * Created by 81062 on 2017/3/15.
+ */
+public class IOrderApplication extends ResourceConfig {
+
+    public IOrderApplication() {
+        packages("cn.net.normcore.iorder.api");
+        register(LoggingFeature.class);
+        property(ServerProperties.TRACING, TracingConfig.ON_DEMAND.name());
+    }
+}
---
+++ iOrder_Server_New/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 81062
+  Date: 2017/3/15
+  Time: 19:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>iOrder</title>
+</head>
+<body>
+<h1>iOrder</h1>
+</body>
+</html>
