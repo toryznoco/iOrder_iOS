75d223aaacd30d783fc13163a7540fd62cc07929
Tory <342410202@qq.com>
2017-03-22 14:07:09 +0800

    修改Tool为Utilities，添加IONetworkTool

---
+++ iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.h
@@ -0,0 +1,15 @@
+//
+//  IONetworkTool.h
+//  iOrder
+//
+//  Created by Tory on 22/03/2017.
+//  Copyright © 2017 normcore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IONetworkTool : NSObject
+
+
+
+@end
---
+++ iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.m
@@ -0,0 +1,13 @@
+//
+//  IONetworkTool.m
+//  iOrder
+//
+//  Created by Tory on 22/03/2017.
+//  Copyright © 2017 normcore. All rights reserved.
+//
+
+#import "IONetworkTool.h"
+
+@implementation IONetworkTool
+
+@end
