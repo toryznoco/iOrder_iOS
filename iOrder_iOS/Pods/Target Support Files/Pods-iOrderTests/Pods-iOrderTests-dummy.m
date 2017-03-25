ec00ffc86747bdfedeaf3bab7d2048f2278183f2
Tory <342410202@qq.com>
2017-03-25 19:31:22 +0800

    Revert "取消版本框架指定"
    This reverts commit 534b74bc8638ca663b498b2a047a809f686aefb7.

--- iOrder_iOS/Pods/Target Support Files/Pods-iOrderTests/Pods-iOrderTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOrderTests : NSObject
-@end
-@implementation PodsDummy_Pods_iOrderTests
-@end
--- iOrder_iOS/Pods/Target Support Files/Pods-iOrderUITests/Pods-iOrderUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOrderUITests : NSObject
-@end
-@implementation PodsDummy_Pods_iOrderUITests
-@end
