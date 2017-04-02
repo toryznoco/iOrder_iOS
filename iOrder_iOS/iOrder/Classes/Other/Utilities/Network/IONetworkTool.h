d11a80c00b21fc64fc3dd118d3ee58854467e1e2
Tory <342410202@qq.com>
2017-04-02 23:15:48 +0800

    完成tokenPOST

--- iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.h
+++ iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.h
@@ -22,6 +22,7 @@
 
 /**
  Get请求
+ 在App启动时需要调用startMonitoring方法监控网络状态
 
  @param URLString URL
  @param parameters 参数
@@ -38,6 +39,7 @@
  Get请求带accessToken
  先判断accessToken，未过期就正常请求，accessToken放在了请求头中
  过期则请求刷新accessToken再请求数据
+ 在App启动时需要调用startMonitoring方法监控网络状态
  
  @param URLString URL
  @param parameters 参数
@@ -52,6 +54,7 @@
 
 /**
  Post请求
+ 在App启动时需要调用startMonitoring方法监控网络状态
 
  @param URLString URL
  @param parameters 参数
@@ -64,9 +67,26 @@
                        success:(void (^)(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObj))success
                        failure:(void (^)(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error))failure;
 
+/**
+ Post请求带accessToken
+ 先判断accessToken，未过期就正常请求，accessToken放在了请求头中
+ 过期则请求刷新accessToken再请求数据
+ 在App启动时需要调用startMonitoring方法监控网络状态
+ 
+ @param URLString URL
+ @param parameters 参数
+ @param success 请求成功时的回调
+ @param failure 请求失败时的回调
+ @return dataTask
+ */
++ (NSURLSessionDataTask *)tokenPOST:(NSString *)URLString
+                    parameters:(id)parameters
+                       success:(void (^)(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObj))success
+                       failure:(void (^)(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error))failure;
 
 /**
  发送格式为JSON的POST请求
+ 在App启动时需要调用startMonitoring方法监控网络状态
 
  @param URLString url
  @param parameters 参数
--- iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.m
+++ iOrder_iOS/iOrder/Classes/Other/Utilities/Network/IONetworkTool.m
@@ -166,6 +166,86 @@ extern BOOL ifNeededRefreshAccessToken;
 }
 
 /**
+ Post请求带accessToken
+ 先判断accessToken，未过期就正常请求，accessToken放在了请求头中
+ 过期则请求刷新accessToken再请求数据
+ 在App启动时需要调用startMonitoring方法监控网络状态
+ 
+ @param URLString URL
+ @param parameters 参数
+ @param success 请求成功时的回调
+ @param failure 请求失败时的回调
+ @return dataTask
+ */
++ (NSURLSessionDataTask *)tokenPOST:(NSString *)URLString
+                         parameters:(id)parameters
+                            success:(void (^)(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObj))success
+                            failure:(void (^)(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error))failure {
+    
+    // 检查网络是否可用
+    if (![AFNetworkReachabilityManager sharedManager].reachable) {
+        IOLog(@"%ld", [AFNetworkReachabilityManager sharedManager].networkReachabilityStatus);
+        // 若不可用，则抛出错误
+        if (failure) {
+            failure(nil, [IOError errorWithCode:IOErrorCodeNetworkDisable description:kIOStringNetworkDisable]);
+        }
+        return nil;
+    }
+    
+    AFHTTPSessionManager *mgr = [AFHTTPSessionManager manager];
+    
+    __block BOOL ifSaveAccessTokenSucceed = NO;
+    
+    // 检查token是否有效
+    if ([IOAccountTool checkIfAccessTokenValid]) {
+        // token有效，在请求头中带上token发送请求
+        [mgr.requestSerializer setValue:[IOAccountTool accessToken] forHTTPHeaderField:@"token"];
+        // 用AFNetworking发送请求
+        return [mgr POST:URLString parameters:parameters progress:nil success:success failure:failure];
+        
+    } else {
+        // 创建信号量
+        dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_async(queue, ^{
+            // token无效，则请求刷新token
+            NSString *urlStr = [NSString stringWithFormat:@"%@%@", kIOHTTPBaseUrl, kIOHTTPAccessTokenUrl];
+            
+            [mgr POST:urlStr parameters:[IOAccountTool refreshToken] progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                IOAccessTokenResult *result = [IOAccessTokenResult mj_objectWithKeyValues:responseObject];
+                if (result.result) {
+                    // 保存accessToken
+                    [IOAccountTool saveAccessToken:result.accessToken validTime:result.accessTokenTTL];
+                    ifSaveAccessTokenSucceed = YES;
+                } else {
+                    IOLog(@"token刷新失败，%@", result.message);
+                    if (failure) {
+                        failure(nil, [IOError errorWithCode:result.code description:result.message]);
+                    }
+                }
+                dispatch_semaphore_signal(sema);
+            } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                IOLog(@"%@", error);
+                if (failure) {
+                    failure(task, error);
+                }
+                dispatch_semaphore_signal(sema);
+            }];
+        });
+        // 等待信号量变为1再继续请求数据
+        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+        
+        if (ifSaveAccessTokenSucceed) {
+            // 用新的acccessToken请求数据
+            [mgr.requestSerializer setValue:[IOAccountTool accessToken] forHTTPHeaderField:@"token"];
+            // 用AFNetworking发送请求
+            return [mgr POST:URLString parameters:parameters progress:nil success:success failure:failure];
+        }
+        return nil;
+    }
+}
+
+/**
  发送格式为JSON的POST请求
  
  @param URLString url
